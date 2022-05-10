00942fbc9c822ddd86737a48e609c73536e18642
Pedro Munhoz <pedro.munhoz@gmail.com>
2022-05-09 21:36:00 -0300

    migration CustomMessageTitleSupport

---
+++ src/api/Migrations/20220510003158_CustomMessageTitleSupport.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class CustomMessageTitleSupport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomTitles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    OldState = table.Column<string>(type: "text", nullable: true),
+                    NewState = table.Column<string>(type: "text", nullable: true),
+                    WebhookType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomTitles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomTitles");
+        }
+    }
+}
